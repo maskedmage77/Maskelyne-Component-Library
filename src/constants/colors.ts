b6ce88abbdc20e63e8911346a27b817ac91d6558
Jacob Hines <maskedmage77@gmail.com>
2021-10-19 14:26:57 -0400

    changed card background to solid color

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -3,6 +3,6 @@ export const accentColor = "#bb21ff";
 export const headerColor = "#ffffff";
 export const textColor = "#989898";
 export const dividerColor = "#686868";
-export const cardColor = "#202020";
+export const cardColor = "#101010";
 export const cardColorDark = "#181818";
 export const codeFontColor = "#21FF48";
